5c275fc94a75908332fc8dc6521be5ed86df29c4
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-14 20:25:00 +1000

    Remove old, unused version of Chinese vocab import.
    Sometimes if you want something done properly, you have to do it
    yourself.

--- minerva/management/commands/cz.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from optparse import make_option
-
-class Command(BaseCommand):
-    help = "Imports simplified chinese from the simplified chinese character file"
-    args = '[file name]'
-
-    def handle(self, addrport='', *args, **options):
-        pass
