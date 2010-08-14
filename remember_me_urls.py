a1866347235041cb0e4e95b403bfb5e996221e18
Ying Stevens <ying.stevens@gmail.com>
2010-08-14 22:55:50 +1000

    Revised static template files setup.

--- remember_me_urls.py
+++ remember_me_urls.py
@@ -1,10 +1,17 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from django.views.static import serve
+import os
+import minerva.urls
 
 admin.autodiscover()
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^static/(.*)$', serve, {'document_root': os.path.join(CURRENT_DIR, "static/")}),
+    (r'^$', include(minerva.urls)),
 )
 
