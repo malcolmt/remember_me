8c28895b1dabab5f53a0e71c37342074b4270821
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-14 23:54:40 +1000

    Small changes based on a little code review.

--- minerva/questions.py
+++ minerva/questions.py
@@ -1,5 +1,6 @@
-from minerva.models import Word
 import random
+
+from minerva.models import Word
 
 QUESTION_SCENARIOS = (
     # Question, answer
@@ -9,16 +10,20 @@ QUESTION_SCENARIOS = (
 )
 
 def create_question(user, language, level):
+    """
+    Returns a question and multiple plausible answers, given the user, their
+    preferred language and targeted skill level.
+
+    Right now, this is very simple (doesn't take into account the user's
+    performance at all). Eventually, it will be a graded selection based on
+    what the user finds difficult, etc.
+    """
     # FIXME - maybe sanity check language
+    # FIXME: Cache this, for each language.
     pks = Word.objects.filter(language=language, level=level).values_list("pk", flat=True)
-
-    sampled_pks = random.sample(pks, 4)
-
-    sampled_words = Word.objects.filter(pk__in=sampled_pks)
-
+    sampled_words = Word.objects.filter(pk__in=random.sample(pks, 4))
     question_attribute, answer_attribute = random.choice(QUESTION_SCENARIOS)
-
-    question = sampled_words[0].__getattribute__(question_attribute)
-    answers = [i.__getattribute__(answer_attribute) for i in sampled_words]
+    question = getattr(sampled_words[0], question_attribute)
+    answers = [getattr(item, answer_attribute) for item in sampled_words]
 
     return question, answers
