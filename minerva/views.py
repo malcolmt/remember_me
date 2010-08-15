6ee6bea9618e89283c75c57e952e7f91a01f7b76
Matthew Stevens <matthew.stevens@gmail.com>
2010-08-15 17:45:48 +1000

    Added a simple way to show last question result

--- minerva/views.py
+++ minerva/views.py
@@ -8,7 +8,9 @@ from minerva.forms import QuestionForm
 def validate_answer(request):
     """
     For now just update the correct answer with the data
+    FIXME - how do I get a question form to validate across fields and against the db
     """
+    msg = ""
     form = QuestionForm(request.POST)
     if not form.is_valid():
         # FIXME: Hmm ... form tampering?! Anything legit? What to do?
@@ -25,11 +27,19 @@ def validate_answer(request):
     progress.attempts += 1
     if int(form.cleaned_data['answer']) == (word.pk):
         progress.correct += 1
+        msg = "Correct. %s = %s" % (word.word, word.meaning)
+    else:
+        msg = "Wrong! %s = %s" % (word.word, word.meaning)
+        
     progress.save()
+    return msg
         
 def question(request):
+    context = {}
     if request.method == 'POST':
-        validate_answer(request)
+        errors = validate_answer(request)
+        if errors:
+            context['errors'] = errors
         
     # TODO: Things needed -
     #   - a way to select a language.
@@ -37,10 +47,8 @@ def question(request):
     #   - ...
     problem, answers = create_question(None, "zho", 1)
     form = QuestionForm(question=problem, answers = answers)
-    context = {
-        'question': problem[1],
-        'form': form,
-        }
+    context['question'] = problem[1]
+    context['form'] = form
     return render_to_response('minerva/question.html', context,
             RequestContext(request))
 
