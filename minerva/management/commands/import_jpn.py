bde826ebae4c40c1bf33748a8f39f742b4fe3ea6
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-15 13:33:16 +1000

    Added finer grading possibilities for vocabularies.
    This adds an optional sub_level field to the Word model. We also import
    the number of strokes in a Japanese character to give the sub-level for
    JPN (fewer strokes are presumably easier characters to remember).

--- minerva/management/commands/import_jpn.py
+++ minerva/management/commands/import_jpn.py
@@ -3,6 +3,7 @@ Import the standard Japanese vocabulary list.
 """
 
 import os
+from optparse import make_option
 
 from django.core.management import base
 from BeautifulSoup import BeautifulSoup
@@ -14,27 +15,41 @@ LANG_CODE = "jpn"
 class Command(base.BaseCommand):
     help = "Import Japanese vocabulary list"
     args = "[filename]"
+    option_list = base.BaseCommand.option_list + (
+        make_option("--strokes", action="store_true", dest="strokes",
+            default=False,
+            help="Import stroke count as sub-level in Word model."),
+    )
 
     def handle(self, *args, **options):
         if len(args) != 1:
             raise base.CommandError("Usage is import_jpn %s" % self.args)
 
         verbosity = int(options.get("verbosity", 0))
+        strokes = options.get("strokes", False)
         source_file = os.path.join(os.getcwd(), args[0])
         soup = BeautifulSoup(open(source_file).read())
-        existing = set(models.Word.objects.filter(language=LANG_CODE). \
-                values_list("word", flat=True))
+        old_data = models.Word.objects.filter(language=LANG_CODE). \
+                values_list("word", "level", "sub_level", "id")
+        existing = dict([(elt[0], elt[1:]) for elt in old_data])
         for row in soup.findAll("tr"):
             pos = row.td.findNextSiblings("td")
             word = pos[0].string.strip()
-            if word in existing:
-                continue
+            strokes = int(pos[3].string)
             level = pos[4].string.strip()
             if level == "S":
                 level = 7
             else:
                 level = int(level)
+            data = existing.get(word)
+            if data:
+                if data[0] == level and data[1] == strokes:
+                    continue
+                else:
+                    pk = data[2]
+            else:
+                pk = None
             meaning = pos[5].string.strip()
-            models.Word(word=word, level=level, meaning=meaning,
-                    language=LANG_CODE).save()
+            models.Word(id=pk, word=word, level=level, sub_level=strokes,
+                    meaning=meaning, language=LANG_CODE).save()
 
