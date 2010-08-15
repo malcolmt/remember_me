efd728d7733fcece783a09800017e9d29859f03a
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-15 21:32:43 +1000

    Allow for cancelling a login attempt via the dropdown.

--- static/js/login.js
+++ static/js/login.js
@@ -7,9 +7,15 @@ function hide_login_form() {
 	$("#login-form:visible").hide("blind");
 }
 
+function cancel_login() {
+	$("#login-form:visible").hide("blind");
+    return false;
+}
+
 function login_setup() {
 	$("#login-link").click(show_login_form);
 	$("#login-form input").click(hide_login_form);
+    $("#login-cancel").click(cancel_login);
 }
 
 $(login_setup);
