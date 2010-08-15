30989dfd2273e698de8a227580376e7b079561aa
Matthew Stevens <matthew.stevens@gmail.com>
2010-08-15 23:57:04 +1000

    Fixd profile creation to make language selection optional

--- user_management/views.py
+++ user_management/views.py
@@ -3,17 +3,19 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from minerva.models import UserProfile
 from minerva.forms import UserProfileForm
 
 def create_user(request):
->>>>>>> 682ec4d6955a55ca02e446c146e15f7c7a5e2898
     if request.method == 'POST':
         user_create_form = UserCreationForm(request.POST)
         user_profile_form = UserProfileForm(request.POST)
         if user_create_form.is_valid():
-            user_create_form.save()
+            user = user_create_form.save()
             if user_profile_form.is_valid():
-                user_profile_form.save()
+                profile = UserProfile.objects.get(student=user)
+                profile.language = user_profile_form.cleaned_data['language']
+                profile.save()
             return HttpResponseRedirect('/login/')
     else:
         user_create_form = UserCreationForm()
