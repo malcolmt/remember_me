2d651bd82b02c6476cf7f1be92eff1693c514319
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-15 23:05:57 +1000

    Register the UserProfile admin class with the admin app.

--- minerva/admin.py
+++ minerva/admin.py
@@ -12,10 +12,11 @@ class ProgressAdmin(admin.ModelAdmin):
     # TODO: Make a column that is "correct / attempts" for readability.
     list_filter = ["student"]
 
-class UserProfile(admin.ModelAdmin):
+class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("student", "language")
     list_filter = ["language"]
 
 admin.site.register(models.Word, WordAdmin)
 admin.site.register(models.Progress, ProgressAdmin)
+admin.site.register(models.UserProfile, UserProfileAdmin)
 
