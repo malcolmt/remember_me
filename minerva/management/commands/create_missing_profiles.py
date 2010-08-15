947529851d69fd3d9aa5779f14c01d14337734af
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-15 23:06:50 +1000

    Added management command to populate user profiles.

---
+++ minerva/management/commands/create_missing_profiles.py
@@ -0,0 +1,22 @@
+"""
+Create initial UserProfile classes for any users in the system that don't
+already have one.
+"""
+
+from django.contrib.auth import models as auth_models
+from django.core.management import base
+
+from minerva import models
+
+class Command(base.BaseCommand):
+    help = "Create UserProfiles for any Users that don't have one."
+
+    def handle(self, *args, **options):
+        if len(args):
+            raise base.CommandError("Usage is create_user_profiles.")
+
+        verbosity = int(options.get("verbosity", 0))
+        for user in auth_models.User.objects.filter(userprofile=None):
+            if verbosity > 0:
+                print "Create profile for %s" % user
+            models.UserProfile(student=user).save()
