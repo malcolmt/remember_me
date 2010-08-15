c95521a6c802aba0185a4b868d1575e67ed2083c
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-15 22:37:17 +1000

    Simplify the standlone login form.
    It now reuses the {% login_form %} template tag.

--- user_management/templatetags/auth_tags.py
+++ user_management/templatetags/auth_tags.py
@@ -4,6 +4,9 @@ from django.contrib.auth import forms
 register = template.Library()
 
 @register.inclusion_tag("user_management/login_form.html")
-def login_form():
-    return {"form": forms.AuthenticationForm()}
+def login_form(style=None):
+    context = {"form": forms.AuthenticationForm()}
+    if style != "visible":
+        context["hidden"] = True
+    return context
 
