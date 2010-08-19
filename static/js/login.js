8a8a760836bfa3ad87cc30bef03f9119a2c4e907
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-08-19 15:46:30 +1000

    Login dropdown display improvements.
    - Fixes github issue 14.
    - No longer shifts the toolbar links.
    - Drops down, instead of appears from top-right corner.
    - Background is opaque so that underneath content doesn't show through.
    - Has a border around the box now.

--- static/js/login.js
+++ static/js/login.js
@@ -1,14 +1,14 @@
 function show_login_form() {
-	$("#login-form").show("fast");
+	$("#login-form").slideDown(300);
 	return false;
 }
 
 function hide_login_form() {
-	$("#login-form:visible").removeClass("active").hide("slow");
+	$("#login-form:visible").removeClass("active").slideUp(300);
 }
 
 function cancel_login() {
-	$("#login-form:visible").removeClass("active").hide("slow");
+	$("#login-form:visible").removeClass("active").slideUp(300);
 	return false;
 }
 
