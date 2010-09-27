db2e4048a43228fdf65b3dcd8305b70ec2f4b2db
Malcolm Tredinnick <malcolm.tredinnick@gmail.com>
2010-09-27 16:30:30 +1000

    Only install UserProfile when User is created if Minerva is installed.
    Closes issue gh-17.

--- minerva/signal_handlers.py
+++ minerva/signal_handlers.py
@@ -1,9 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
 
 def create_user_profile(sender, **kwargs):
     """
     Create an empty UserProfile whenever a new User is created.
     """
     from minerva import models
+    try:
+        ContentType.objects.get_by_natural_key("minerva", "userprofile")
+    except ContentType.DoesNotExist:
+        # Minerva app hasn't been installed yet.
+        return
     if not kwargs["created"]:
         return
     models.UserProfile(student=kwargs["instance"]).save()
